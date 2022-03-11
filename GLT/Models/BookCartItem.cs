1f17c712fbdf6d0c6a5f4b86a80eabc2644acf53
TaulantAbazi1 <83340571+TaulantAbazi1@users.noreply.github.com>
2022-03-11 13:43:26 +0100

    Add files via upload

---
+++ GLT/Models/BookCartItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GLT.Models
+{
+    public class BookCartItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public Booking Booking { get; set; }
+        public int Amount { get; set; }
+
+
+        public string BookCartId { get; set; }
+    }
+}
