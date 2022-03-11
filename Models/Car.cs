0ae7778cf7969490dfbaf95e25e6c39f3668661b
TaulantAbazi1 <83340571+TaulantAbazi1@users.noreply.github.com>
2022-03-11 13:33:53 +0100

    Add files via upload

---
+++ Models/Car.cs
@@ -0,0 +1,31 @@
+﻿using GLT.Data.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GLT.Models
+{
+    public class Car:IEntityBase
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Car Picture ")]
+        [Required(ErrorMessage = "Profile Picture is required")]
+        public string CarPictureURL { get; set; }
+        [Display(Name = "Brand")]
+        [Required(ErrorMessage = "Full Brand is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Full Name must be between 3 and 50 chars")]
+        public string Brand { get; set; }
+        [Display(Name = "Description")]
+        [Required(ErrorMessage = "Description is required")]
+
+        public string Description { get; set; }
+
+        public List<Car_Booking> Cars_Bookings { get; set; }
+        public string Name { get; internal set; }
+        public string Logo { get; internal set; }
+    }
+}
---
+++ Models/Parking.cs
@@ -0,0 +1,28 @@
+﻿using GLT.Data.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GLT.Models
+{
+    public class Parking:IEntityBase
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Parking Picture")]
+        [Required(ErrorMessage = "Parking Picture is required")]
+        public string Picture { get; set; }
+        [Display(Name = "Parking Name")]
+        [Required(ErrorMessage = "Parking name is required")]
+        public string Name { get; set; }
+        [Display(Name = "Parking Description")]
+        [Required(ErrorMessage = "Parking description is required")]
+        public string Description { get; set; }
+
+        //Relationships 
+        public List<Booking> Bookings { get; set; }
+    }
+}
