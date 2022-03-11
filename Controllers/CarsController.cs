52a5dc6d21d319b91c3174c54540c4fb43e9979c
TaulantAbazi1 <83340571+TaulantAbazi1@users.noreply.github.com>
2022-03-11 13:25:40 +0100

    Add files via upload

---
+++ Controllers/CarsController.cs
@@ -0,0 +1,24 @@
+﻿using GLT.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GLT.Controllers
+{
+    public class CarsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CarsController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var data = _context.Cars.ToList();
+            return View(data);
+        }
+    }
+}
---
+++ Controllers/ParkingsController.cs
@@ -0,0 +1,26 @@
+﻿using GLT.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GLT.Controllers
+{
+    public class ParkingsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ParkingsController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+
+            var allParkings = await _context.Parkings.ToListAsync();
+            return View(allParkings);
+        }
+    }
+}
